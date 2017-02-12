c0af74780767d9ccfff71ab67fbe40ae98047452
Josh Cheek <josh.cheek@gmail.com>
2017-02-11 22:58:22 -0600

    Okay, it works the same as it does in create-react-app

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,12 @@
 const sum = require('./sum');
 
+describe("a", () => {
+  it("passes", () =>
+    expect(true).toEqual(true))
+  it("fails", () =>
+    expect(true).toEqual(false))
+})
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
