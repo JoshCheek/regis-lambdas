09aa92a392072994a7240e595a31e4cad29b1c06
Josh Cheek <josh.cheek@gmail.com>
2017-02-12 00:27:55 -0600

    Switch back to Mocha due to a bug in Jest
    Reported the bug here: https://github.com/facebook/jest/issues/2867

--- sum.test.js
+++ sum.test.js
@@ -1,14 +1,17 @@
-import sum from './sum';
+'use strict'
+
+const assert = require('chai').assert
+const sum    = require('./sum')
 
 describe("quick test", () => {
   it("passes", () =>
-    expect(true).toEqual(true))
+    assert.equal(true, true))
   it("fails", () =>
-    expect(true).toEqual(false))
+    assert.equal(true, false))
   it("passes 2", () =>
-    expect(true).toEqual(true))
+    assert.equal(true, true))
 })
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+it('adds 1 + 2 to equal 3', () => {
+  assert.equal(sum(1, 2), 3);
 });
