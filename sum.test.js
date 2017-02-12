48126b3d851af2750e74f3605829795fbe0d76db
Josh Cheek <josh.cheek@gmail.com>
2017-02-11 23:12:30 -0600

    Hmm, got jest watching the file changes, but not stopping on failure :/

--- sum.test.js
+++ sum.test.js
@@ -3,8 +3,10 @@ import sum from './sum';
 describe("quick test", () => {
   it("passes", () =>
     expect(true).toEqual(true))
-  // it("fails", () =>
-  //   expect(true).toEqual(false))
+  it("fails", () =>
+    expect(true).toEqual(false))
+  it("passes 2", () =>
+    expect(true).toEqual(true))
 })
 
 test('adds 1 + 2 to equal 3', () => {
